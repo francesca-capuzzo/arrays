48faa526d0e39b01845c2e86159217525955266c
Francesca Capuzzo <f.capuzzo9966@gmail.com>
2022-01-24 22:00:23 +0100

    c

---
+++ esercizicasa.js
@@ -0,0 +1,13 @@
+//*******************ESERCIZIO 1******************/
+//SUM OF A RANGE:
+
+// function range1(start, end) {
+//     let rangeArray = [];
+//     for (let i = 0; i <= rangeArray.length; i++) {
+//         const element = rangeArray[i];
+//         rangeArray.push(i);
+//     }
+//     return rangeArray;
+// }
+
+// console.log(range1(1, 10));
